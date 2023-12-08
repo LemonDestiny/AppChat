4745b45fea0c44db78682830190cf8f4178fb4ae
marcos aguilera <maroctw@hotmail.com>
2023-12-07 22:40:59 -0300

    añadiendo funcionalidad similar a un chat

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,18 +1,35 @@
 let input = document.querySelector("input");
 let boton = document.getElementById("botonEnviar");
 let mensajes = document.getElementById("historial");
+let plantilla = `<div class={clase}>
+                  <h3>{nombre}</h3>
+                  <p>{mensaje}</p>
+                </div>`;
 
 boton.addEventListener("click", () => {
-  mensajes.textContent += input.value + "\n";
+  remplazaMensaje("personal", input.value, "Me");
+  remplazaMensaje("otraPersona", "hola carlos todo bien", "Ricardo");
 });
 
 input.addEventListener("keypress", (evento) => {
   if (evento.key === "Enter" && !mensajeVacio(input.value)) {
-    mensajes.textContent += input.value + "\n";
+    remplazaMensaje("personal", input.value, "Me");
+    remplazaMensaje("otraPersona", "hola carlos todo bien", "Ricardo");
     input.value = "";
   }
 });
 
 function mensajeVacio(cadena) {
   return cadena.trim() === "";
+}
+
+function remplazaMensaje(clase, mensaje, persona) {
+  let mensajeNuevo = plantilla
+    .replace("{mensaje}", mensaje)
+    .replace("{nombre}", persona)
+    .replace("{clase}", clase);
+  let contenedor = document.createElement("div");
+  contenedor.innerHTML = mensajeNuevo;
+  mensajes.appendChild(contenedor);
+  mensajes.scrollTo(0, mensajes.scrollHeight);
 }
